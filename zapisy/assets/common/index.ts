2c6b776b2ec42cf52e2c152857a8bbf8904f826a
Kuba Kazimierczak <kuba@kazimierczak.name>
2017-11-20 23:39:54 +0100

    Fix bootstrap

--- zapisy/assets/common/index.ts
+++ zapisy/assets/common/index.ts
@@ -1,4 +1,8 @@
-import * as $ from 'jquery';
-import 'bootstrap';
+import * as $ from "jquery";
+import * as Popper from "popper.js";
 
 (window as any).$ = $;
+(window as any).jQuery = $;
+(window as any).Popper = Popper;
+
+import "bootstrap";
