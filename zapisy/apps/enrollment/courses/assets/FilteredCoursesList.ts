b99ebcb620c9bfb2c86bc578d4c0d83a289a91fa
notechus <seba0713@gmail.com>
2017-11-29 15:18:25 +0100

    async fetch of course list

--- zapisy/apps/enrollment/courses/assets/FilteredCoursesList.ts
+++ zapisy/apps/enrollment/courses/assets/FilteredCoursesList.ts
@@ -1,23 +1,53 @@
 import * as $ from 'jquery';
 
+interface Course {
+    id: number;
+    url: string;
+    name: string;
+}
+
 class FilteredCoursesList {
-    courseList: object[] = [];
     url: string;
 
     constructor(URL: string) {
         this.url = URL;
     }
 
-    fetchCourses() {
+    init() {
+        this.fetchCourses()
+            .then(courses => this.renderCourses(courses.courseList))
+            .then(() => document.getElementById("fetchingListMessage").className += " hidden");
+    }
+
+    protected fetchCourses() {
         return fetch(this.url, {method: 'GET', mode: 'cors'})
             .then(response => {
                 return response.json();
             })
-            .then(courses => this.courseList = courses.courseList)
-            .then(() => console.log(this.courseList));
+    }
+
+    protected createLinkFromCourse(course: Course): Node {
+        let elem: Node = document.createElement("LI");
+        let a = document.createElement("A");
+        a.setAttribute("href", course.url);
+        a.textContent = course.name;
+
+        // let text = document.createTextNode(course.name);
+        elem.appendChild(a);
+
+        return elem;
+    }
+
+    protected renderCourses(courses: Course[]) {
+        let list = document.createElement("UL");
+        list = document.getElementById('courses-list');
+        console.log(courses);
+        courses.forEach(course => {
+            list.appendChild(this.createLinkFromCourse(course));
+        });
     }
 }
 
 $(() => {
-    new FilteredCoursesList("/courses/get_semester_info/" + 338).fetchCourses();
+    new FilteredCoursesList("/courses/get_semester_info/" + 338).init();
 });
