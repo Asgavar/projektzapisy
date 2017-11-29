a3f20d652a087d92448ae6fcf7d08396d49311da
notechus <seba0713@gmail.com>
2017-11-29 22:05:51 +0100

    schedule

--- zapisy/asset-defs.ts
+++ zapisy/asset-defs.ts
@@ -1,18 +1,23 @@
 module.exports = {
-	rawfiles: [
-		{ from: "legacy/css/", to: "css/" },
-		{ from: "legacy/help/", to: "help/" },
-		{ from: "legacy/images/", to: "images/" },
-		{ from: "legacy/js/", to: "js/" },
-		{ from: "legacy/vendor/", to: "vendor/" },
-		{ from: "legacy/favicon.ico", to: "favicon.ico" },
-		{ from: "legacy/feed-icon.png", to: "feed-icon.png" }
-	],
-	bundles: {
+    rawfiles: [
+        {from: "legacy/css/", to: "css/"},
+        {from: "legacy/help/", to: "help/"},
+        {from: "legacy/images/", to: "images/"},
+        {from: "legacy/js/", to: "js/"},
+        {from: "legacy/vendor/", to: "vendor/"},
+        {from: "legacy/favicon.ico", to: "favicon.ico"},
+        {from: "legacy/feed-icon.png", to: "feed-icon.png"}
+    ],
+    bundles: {
         "main": [
-        	"common/_variables.scss",
-        	"common/index.ts",
-			"common/index.scss"
+            "common/_variables.scss",
+            "common/index.ts",
+            "common/index.scss"
         ],
+        "fullcalendar": [
+            "legacy/js/fullcalendar/fullcalendar.js",
+            "legacy/js/fullcalendar/fullcalendar.css",
+            "legacy/js/fullcalendar/gcal.js"
+        ]
     }
 };
