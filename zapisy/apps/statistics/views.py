c38608288dcb7bf35e880b95ff306130c8f00a0b
mateuszu <15063185+mateuszu@users.noreply.github.com>
2019-01-16 07:34:47 +0100

    Start fixing statistics view

--- zapisy/apps/statistics/views.py
+++ zapisy/apps/statistics/views.py
@@ -11,30 +11,33 @@ from apps.enrollment.courses.models.course import CourseEntity, Course
 def main(request):
     return TemplateResponse(request, 'statistics/base.html')
 
-#@permission_required('courses.view_stats')
-# def students(request):
-#    students = Student.objects.get_list_full_info().order_by('t0_min')
-#    return TemplateResponse(request, 'statistics/students_list.html', locals())
-
 
 @permission_required('courses.view_stats')
 def students(request):
-    students = Student.objects.get_list_full_info().\
-        extra(select={'semester_points': "SELECT SUM(value) FROM (SELECT \
-   courses_studentpointsview.value as value \
-FROM courses_course LEFT JOIN courses_group ON (courses_group.course_id = courses_course.id) \
-INNER JOIN records_record ON(records_record.group_id = courses_group.id) \
-LEFT JOIN courses_studentpointsview ON (courses_studentpointsview.entity_id = courses_course.entity_id) \
-WHERE courses_course.semester_id=333 AND courses_studentpointsview.student_id = users_student.id AND records_record.student_id = courses_studentpointsview.student_id AND records_record.status = '1' \
-GROUP BY courses_course.id, courses_studentpointsview.value) as foo"}).\
-        order_by('t0_min')
+    query = """SELECT SUM(value) 
+    FROM (SELECT courses_studentpointsview.value as value
+    FROM courses_course 
+    LEFT JOIN courses_group 
+    ON (courses_group.course_id = courses_course.id)
+    INNER JOIN records_record 
+    ON (records_record.group_id = courses_group.id)
+    LEFT JOIN courses_studentpointsview 
+    ON (courses_studentpointsview.entity_id = courses_course.entity_id)
+    WHERE courses_course.semester_id=333 
+    AND courses_studentpointsview.student_id = users_student.id 
+    AND records_record.student_id = courses_studentpointsview.student_id 
+    AND records_record.status = '1'
+    GROUP BY courses_course.id, courses_studentpointsview.value) as foo"
+    """
+
+   # students = Student.objects.get_list_full_info().extra(select={'semester_points': query}).order_by('t0_min')
     return TemplateResponse(request, 'statistics/students_list.html', locals())
 
 
 @permission_required('courses.view_stats')
 def groups(request):
     semester = Semester.objects.get_next()
-    groups = Group.statistics.in_semester(semester)
+    #groups = Group.statistics.in_semester(semester) #TODO
     return TemplateResponse(request, 'statistics/groups_list.html', locals())
 
 
@@ -74,7 +77,7 @@ def swap(request):
                         lists[g.id].append(r.student)
                         students[r.student_id] = g
                     elif r.status == RecordStatus.QUEUED:
-                        queues[g.id].append(q.student)
+                        queues[g.id].append(r.student)
 
             for group in groups:
                 group.swaps = []
