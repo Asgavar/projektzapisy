8cfb384961d3aa13807d4a0c46da9c964d3ace6d
mateuszu <15063185+mateuszu@users.noreply.github.com>
2019-01-22 23:24:04 +0100

    Remove import that caused incorrect pages size

--- zapisy/assets/common/expose_libs.ts
+++ zapisy/assets/common/expose_libs.ts
@@ -4,6 +4,3 @@ import * as Popper from "popper.js";
 (window as any).$ = $;
 (window as any).jQuery = $;
 (window as any).Popper = Popper;
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
